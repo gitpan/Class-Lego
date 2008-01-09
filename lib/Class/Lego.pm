b6b4bd745306678ff8caefd24138a8c1ed07608a
Adriano Ferreira <ferreira@cpan.org>
2008-01-09 17:41:11 +0000

    Import of FERREIRA/Class-Lego-0.001 from CPAN.
    gitpan-cpan-distribution: Class-Lego
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         FERREIRA/Class-Lego-0.001.tar.gz
    gitpan-cpan-author:       FERREIRA
    gitpan-cpan-maturity:     released

---
+++ lib/Class/Lego.pm
@@ -0,0 +1,26 @@
+
+package Class::Lego;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+Class::Lego - Build your classes with basic building blocks
+
+=head1 SYNOPSIS
+
+  use Class::Lego;
+
+  # soon
+
+=head1 DESCRIPTION
+
+Not ready yet. The first piece is L<Class::Lego::Myself>.
+
+
